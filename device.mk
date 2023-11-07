4554f6a3e350161e23cf8843a34866ede4685829
HELLBOY017 <abhaygill017@gmail.com>
2023-11-07 07:50:40 +0530

    Pong: Import vibrator changes from https://github.com/Pong-development/device_nothing_Pong
    Pong: Checkout vibrator from LA.VENDOR.1.0.r2-06900-WAIPIO.QSSI14.0
    
    Includes:
    https://github.com/AOSPA/android_vendor_qcom-opensource_vibrator/commit/b9d1ef977a88fc5f2fabb54e39dfe9f64b23e99b
    https://github.com/AOSPA/android_vendor_qcom-opensource_vibrator/commit/923706cfe40c5bed70078dcd57ed36fe046905da
    https://github.com/AOSPA/android_vendor_qcom-opensource_vibrator/commit/0225be0747acfab878ac89f0ee0afabb6f60f830
    
    LED Vibrator node has been changed from /sys/class/leds/vibrator to /sys/class/leds/aw_vibrator.
    
    Pong: vibrator: Implement Nothing vibrator effects
    
    [HELLBOY017]: Closely observed by OnePlus's changes in OOS 11, the difference being that our vibrator firmware and kernel support only two wav indexes 1 for short vibrations and 4 for the long ones just like OEMs like motorola.
    
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
    
    Pong: vibrator: Make haptics a bit stronger
    
    Vibrator gain more than 154 creates a slight vibration in the back glass of the device.
    
    Pong: vibrator: Fix misleading indentation
    
    Output:
    device/nothing/Pong/vibrator/Vibrator.cpp:173:13:
    error: misleading indentation; statement is not part of the previous 'if' [-Werror,-Wmisleading-indentation]
                return 0;
                ^
    device/xiaomi/lmi/vibrator/Vibrator.cpp:171:9: note: previous statement is here
            if (playLengthMs != NULL)
            ^
    1 error generated.
    
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    
    Pong: vibrator: Bring vibration intensity support
    
    Pong: vibrator: Reduce gain for 100ms+ duration haptics
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    
    Pong: Increase gain for 100ms+ haptics
    
    - 96 > 126
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    
    Pong: vibrator: Report vibration intensity capibilities regardless of vibrator type
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    
    Pong: vibrator: Fixup intensity value
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    
    Pong: vibrator: Increase vmax gain
    
    OEMs like oppo use 9.408V as max vmax gain.
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    
    Pong: vibrator: Switch back to 0x80 vibrator gain
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    
    Pong: vibrator: Improve amplitude handling
    
    Revert back to our old way of handling timeoutMs.
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    
    Pong: vibrator: Make haptic stronger & Use correct voltage for long duration haptics
    
    Sourced from oplus sm8250 source.
    
    Pong: vibrator: Make haptics a bit stronger

--- device.mk
+++ device.mk
@@ -417,7 +417,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.nt.pong
+    vendor.qti.hardware.vibrator.service.nt
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
