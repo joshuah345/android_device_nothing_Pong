9204636ed4f88b2634d820f16984d5ddf76e3d78
hiroshi <hiroshi@tanuki.page>
2025-05-15 08:15:32 +0100

    Pong: set TARGET_HAS_UDFPS
    Required to show UDFPS customization settings

--- device.mk
+++ device.mk
@@ -154,6 +154,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.nothing
 
+TARGET_HAS_UDFPS := true
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
