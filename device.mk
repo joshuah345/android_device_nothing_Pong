ae964d0b0f440809e296cb0139218220e632736d
Ghosuto <clash.raja10@gmail.com>
2025-06-09 10:39:50 +0000

    Pong: Stub out some symbols from libofflineproc_jni.so
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- device.mk
+++ device.mk
@@ -394,6 +394,10 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom-caf/bootctrl \
     hardware/qcom-caf/common/libqti-perfd-client 
 
+# Shims
+PRODUCT_PACKAGES += \
+    lib_shim_ntcamera
+
 # Task Profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
