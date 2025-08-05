Neoteric-OS/device_nothing_Pong

Pong: Add support for alternate display ids
Recent kernel change in the display driver [1] caused a change in
display hardware port id which resulted in a change in display id.
Add support for both variations of display ids.

[1]: https://git.codelinaro.org/clo/la/platform/vendor/opensource/display-drivers/-/commit/0a3ce4196429ef713359548a99212dc0a4ec4d9a

Change-Id: I58d3cb151ce3683643c6094758bee0a66bfa8cd5
Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

device.mk
@@ -99,6 +99,7 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/displayconfig/display_id_4630946639017191810.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946639017191809.xml \
     $(DEVICE_PATH)/configs/displayconfig/display_id_4630946639017191810.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946639017191810.xml
 
 PRODUCT_PACKAGES += \

joshuah345/android_device_nothing_Pong

device.mk
@@ -138,6 +138,7 @@ $(call soong_config_set,camera,override_format_from_reserved,true)
 
 # Display
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_4630946639017191810.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946639017191809.xml \
     $(LOCAL_PATH)/configs/displayconfig/display_id_4630946639017191810.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946639017191810.xml
 
 PRODUCT_PACKAGES += \