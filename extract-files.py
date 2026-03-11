2e95527c7acaae5d40d42b7f815d1a875eec6974
hiroshi <hiroshi@tanuki.page>
2026-03-11 02:42:43 -0400

    Pong: Add device tree to vendor namespace imports
    * Used for nt camera stuff

--- extract-files.py
+++ extract-files.py
@@ -19,6 +19,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
+    'device/nothing/Pong',
     'hardware/qcom-caf/sm8450',
     'hardware/qcom-caf/wlan',
     'vendor/qcom/opensource/commonsys/display',
