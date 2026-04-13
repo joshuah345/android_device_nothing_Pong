e262376016f6bcbef4788f4692d1cc1f6020db93
hiroshi <hiroshi@tanuki.page>
2026-04-12 23:33:00 -0400

    Pong: Build LiveDisplay SunlightEnhancement

--- device.mk
+++ device.mk
@@ -249,6 +249,12 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/charging_en)
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay-service.sysfs
+
+$(call soong_config_set_bool,livedisplay_sysfs,enable_se,true)
+
 # Logging
  SPAMMY_LOG_TAGS := \
     Diag_Lib \
